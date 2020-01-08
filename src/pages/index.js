3e16bf55b817c4d64bf37ce6ff5771d6fe80cb0e
Jan Krems <jankrems@google.com>
2020-01-08 08:47:46 -0800

    chore: Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Embrace JS" />
+    <h1>Embrace JS</h1>
+  </Layout>
+);
+
+export default IndexPage;
